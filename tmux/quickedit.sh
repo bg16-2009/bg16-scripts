e4ea974e3c4f95799bd6fa1df57fb1113c49052a
Bogdan Gabor <bogdan.gabor.cj@gmail.com>
2024-04-11 15:00:41 +0300

    Improved tmux integration

--- tmux/quickedit.sh
+++ tmux/quickedit.sh
@@ -7,12 +7,17 @@ filetypes["Text"]=".txt"
 filetypes["Cpp"]=".cpp"
 filetypes["Python"]=".py"
 
-selected_filetype=${filetypes[$(printf '%s\n' "${!filetypes[@]}" | fzf)]}
+selected_filetype=${filetypes[$(printf '%s\n' "${!filetypes[@]}" | fzf-tmux)]}
 
 file="/tmp/edit$selected_filetype"
 
 touch "$file"
 
-nvim $file
+if [ "$1" == "popup" ]; then
+	tmux popup -E nvim $file
+fi
+if [ "$1" == "window" ]; then
+	tmux new-window nvim $file
+fi
 cat $file | tmux load-buffer -
 rm $file
