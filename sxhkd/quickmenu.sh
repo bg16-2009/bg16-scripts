2a0342182c135ca88e573084998ce5a47c498cb3
Bogdan Gabor <bogdan.gabor.cj@gmail.com>
2024-05-13 21:49:16 +0300

    Add lock functionality

--- sxhkd/quickmenu.sh
+++ sxhkd/quickmenu.sh
@@ -5,6 +5,7 @@ declare -A commands
 commands["Change brighteness"]="brighteness"
 commands["Change volume"]="vol"
 commands["Cht.sh"]="chtsh"
+commands["Lock"]="lock"
 
 selected=${commands[$(printf '%s\n' "${!commands[@]}" | rofi -dmenu -i -p "")]}
 
@@ -18,6 +19,10 @@ case "$selected" in
 	current_brightness=$(brightnessctl -m | awk -F, '{print $4}')
 	new_brightness=$(echo "" | rofi -dmenu -theme-str 'listview {enabled: false;}' -i -p "Enter brightness percentage(current is $current_brightness)")
 	brightnessctl s "$new_brightness%"
+	;;
+"lock")
+	sleep 0.3
+	i3lock -B 10 --indicator -k --time-color=FFFFFF --date-color=FFFFFF --time-str="%H:%M"
 	;;
 "chtsh")
 	# TODO
